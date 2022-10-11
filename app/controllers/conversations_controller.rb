dbfd7b84b0cdb1871727b3f0d77fec609d8e0eae
May Four Apple Espinosa <mayfourapple@gmail.com>
2022-10-11 19:58:04 +0800

    Fix Messages routes in navbar

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,16 +1,16 @@
 class ConversationsController < ApplicationController
   def index
-    @conversations = Conversation.where(user_1: current_user).or(user_2: current_user)
+    @conversations = Conversation.where(user_1_id: current_user.id).or(Conversation.where(user_2_id: current_user.id))
   end
 
   def new
-    @conversation = Conversation.create(user_1: current_user, user_2_id: params[:recipient_id])
+    @conversation = Conversation.create(user_1_id: current_user.id, user_2_id: params[:recipient_id])
     
     redirect_to conversation_path(@conversation)
   end
 
   def show
-    @conversations = Conversation.where(user_1: current_user).or(Conversation.where(user_2: current_user))
+    @conversations = Conversation.where(user_1_id: current_user.id).or(Conversation.where(user_2_id: current_user.id))
     @conversation = Conversation.find(params[:id])
     @message = Message.new
   end
